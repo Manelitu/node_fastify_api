a3cf83cfa8f1b08a423678b7baee0c96f6417fd8
Manelitu <emanuelwork1@gmail.com>
2023-02-12 10:16:54 -0300

    refactor: change port to env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Http server running'))
