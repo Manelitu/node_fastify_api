6c06f748ca312735c1cf7f3df61a742097c5d686
Manelitu <emanuelwork1@gmail.com>
2023-02-12 19:20:22 -0300

    feat: create mount summary transaction by id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -25,6 +25,15 @@ export async function transactionRoutes(app: FastifyInstance) {
     return { transaction }
   })
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
