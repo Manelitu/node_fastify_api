2b7bae1a7558b64b76da414aa35b3f821c0f62bc
Manelitu <emanuelwork1@gmail.com>
2023-02-12 19:16:51 -0300

    feat: create list transaction by id route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,7 +7,22 @@ export async function transactionRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*')
 
-    return transactions
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where('id', id)
+      .first()
+
+    return { transaction }
   })
 
   app.post('/', async (request, reply) => {
